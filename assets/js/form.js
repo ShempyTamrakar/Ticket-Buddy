8e9e1c5d43f19a17c2afbc10a87de21536e80346
Shempy Tamrakar <tamrakarshempy@gmail.com>
2021-12-17 21:47:31 +0530

    modified login page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,42 @@
-function showPassword() {
-    var password = document.getElementById('pwd');
-    if (password.type === 'password') {
-        password.type = "text";
-    } else {
-        password.type = "password";
+$('.form').find('input, textarea').on('keyup blur focus', function(e) {
+
+    var $this = $(this),
+        label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.removeClass('highlight');
+        }
+    } else if (e.type === 'focus') {
+
+        if ($this.val() === '') {
+            label.removeClass('highlight');
+        } else if ($this.val() !== '') {
+            label.addClass('highlight');
+        }
     }
-}
+
+});
+
+$('.tab a').on('click', function(e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+});
